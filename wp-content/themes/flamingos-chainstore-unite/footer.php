a10e629fccae7bdb8d04b5b5e1b5d4be9e37348f
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-04 00:18:50 +0900

    フッターいらない文字を削る

--- wp-content/themes/flamingos-chainstore-unite/footer.php
+++ wp-content/themes/flamingos-chainstore-unite/footer.php
@@ -10,21 +10,13 @@
             </div><!-- row -->
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info container">
-			<div class="row">
-				<nav role="navigation" class="col-md-6">
-					<?php unite_footer_links(); ?>
-				</nav>
+	<div id="colophon" class="site-footer" role="contentinfo">
 
-				<div class="copyright col-md-6">
-					<?php do_action( 'unite_credits' ); ?>
-					<?php echo of_get_option( 'custom_footer_text', 'unite' ); ?>
-					<?php do_action( 'unite_footer' ); ?>
-				</div>
-			</div>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+フッター
+
+
+
+	</div><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
