923093ff8f27a6ab507117f2cbd311c94aa84385
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-06 16:25:35 +0900

    フッターのメルマガ登録設定　２段用と分けて作った

--- wp-content/themes/flamingos-chainstore-unite/mailmag-2dan.php
+++ wp-content/themes/flamingos-chainstore-unite/mailmag-2dan.php
@@ -1,13 +1,18 @@
 <div class="row mailmagpadding">
   <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
 
-<form action="https://24auto.biz/ichigo-branding/planmail.php" method="post" enctype="multipart/form-data">
+<form action="https://88auto.biz/※" method="post" enctype="multipart/form-data">
 <input type="hidden" name="mcode" value="UTF-8">
-<input type="hidden" name="pid" value="14">
+<input type="hidden" name="pid" value="21">
 <input type="hidden" name="spflg" value="1">
 <table width="520" cellpadding="3" cellspacing="1" border="0" bgcolor="#aaaaaa" class="OM_table">
 <tr>
 <td align="center" colspan="2" class="bizfom3"><b>ステップメール・メルマガ登録フォーム（全ページ掲載）</b><br></td>
+</tr>
+<tr>
+<td bgcolor="#fafafa" align="center" width="120"><b>お名前</b><br><span class="fna">(名字のみ)</span><strong style="color:#FF0000;"><span class="fna">*</span></strong></td>
+<td bgcolor="#fafafa" width="400"> <input type="text" name="name1" size="30" class="bizfom2">
+</td>
 </tr>
 <tr>
 <td bgcolor="#fafafa" align="center" width="120"><b>メールアドレス</b><strong style="color:#FF0000;"><span class="fna">*</span></strong></td>
