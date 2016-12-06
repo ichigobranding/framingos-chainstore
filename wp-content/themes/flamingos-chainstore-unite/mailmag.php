a117bb1ca5d562b19e009e0199f7b37456e7ef96
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-06 13:40:13 +0900

    フッターメルマガ登録追加

---
+++ wp-content/themes/flamingos-chainstore-unite/mailmag.php
@@ -0,0 +1,43 @@
+<div class="row mailmagpadding">
+  <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+
+
+<style type="text/css">
+<!--
+.OM_table{width:100%;border-collapse:collapse;border:1px solid #c41840;background-color:#c41840;}
+.OM_table tr td{text-align:center;}
+.bizfom{margin: 10px 0 0 0;}
+.bizfom2{margin: 10px 0 13px 0;}
+.bizfom3{color: #fff;font-size: 14px;padding: 10px 5px;}
+//-->
+</style>
+
+<form action="https://88auto.biz/smaps/planmail.php" method="post" enctype="multipart/form-data">
+<input type="hidden" name="mcode" value="UTF-8">
+<input type="hidden" name="pid" value="21">
+<input type="hidden" name="spflg" value="1">
+<table width="520" cellpadding="3" cellspacing="1" border="0" bgcolor="#aaaaaa" class="OM_table">
+<tr>
+<td align="center" colspan="2" bgcolor="#c41840" class="bizfom3"><b>起業や独立開業の実現に向けたファーストステップ１０日間無料メールセミナー</b><br></td>
+</tr>
+<tr>
+<td bgcolor="#fafafa" align="center" width="120"><b>お名前</b><br><span class="fna">(名字のみ)</span><strong style="color:#FF0000;"><span class="fna">*</span></strong></td>
+<td bgcolor="#fafafa" width="400"> <input type="text" name="name1" size="30" class="bizfom2">
+</td>
+</tr>
+<tr>
+<td bgcolor="#fafafa" align="center" width="120"><b>メールアドレス</b><strong style="color:#FF0000;"><span class="fna">*</span></strong></td>
+<td bgcolor="#fafafa" width="400"> <input type="text" name="email" size="30" class="bizfom2">
+</td>
+</tr>
+</table>
+<table width="520">
+<tr>
+<td align="center"></td>
+</tr>
+</table>
+<center><input type="submit" name="sbm" value="登録"></center>
+</form>
+
+  </div>
+</div>
