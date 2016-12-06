7aa2a06d770db54c552041aa1f604d5a33c46c56
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-06 17:31:15 +0900

    ヘッダーの右側の連絡枠

--- wp-content/themes/flamingos-chainstore-unite/header.php
+++ wp-content/themes/flamingos-chainstore-unite/header.php
@@ -48,7 +48,7 @@
 
 			<div class="col-md-3 col-md-offset-3">
 									<!-- 連絡先 -->
-												<div class="tellwaku well">
+												<div class="tellwaku">
 													<span class="uke2">地域名・キーワード</span><br>
 													<span class="glyphicon glyphicon-earphone telcon" aria-hidden="true"></span><a href="tel:0120916625">0120-916-625</a><br>
 													<span class="glyphicon glyphicon-envelope telcon" aria-hidden="true"></span><a href="<?php echo esc_url( home_url( '/' ) ); ?>/contact">メール受付はコチラ</a><br>
