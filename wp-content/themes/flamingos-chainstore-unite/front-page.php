e7da9462b0a9bb1dd6432aa07c72a6d2fbdf7e34
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-09 07:50:35 +0900

    メッセージボード　ウィジェット

--- wp-content/themes/flamingos-chainstore-unite/front-page.php
+++ wp-content/themes/flamingos-chainstore-unite/front-page.php
@@ -33,6 +33,7 @@
 				<div class="row">
 						<div class="col-md-6 col-md-push-3">
 							 <?php get_template_part( 'messageborad' ); ?>
+							 <?php get_template_part( 'messageborad-widget' ); ?>
 							 <?php get_template_part( 'semi-indexparts-main' ); ?>
 							 <?php get_template_part( 'consul-indexparts-dw' ); ?>
 							 <?php get_template_part( 'blogul' ); ?>
---
+++ wp-content/themes/flamingos-chainstore-unite/messageborad-widget.php
@@ -0,0 +1,15 @@
+<div class="skinMessageBoard-widgetcss" >
+
+<!-- 新着情報 -->
+
+     <div class="skinMessageBoard3-widgetcss" >
+    <!--フリースペース-->
+    widget
+      <?php if ( ! dynamic_sidebar( 'messageboard-widget' ) ) : ?>
+      <?php endif; // end sidebar widget area ?>
+    <!--フリースペース-->
+    </div>
+      
+<!-- 新着情報 -->
+
+</div>
