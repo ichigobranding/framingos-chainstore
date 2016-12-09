6ac8d6cfb48946fda24ca15ac2773f30837722d5
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-10 01:38:09 +0900

    front-page　マンプ読み込まない

--- wp-content/themes/flamingos-chainstore-unite/front-page-hensyu.php
+++ wp-content/themes/flamingos-chainstore-unite/front-page-hensyu.php
@@ -36,7 +36,7 @@
 							 <?php get_template_part( 'messageborad-widget' ); ?>
 							 <?php get_template_part( 'semi-indexparts-main' ); ?>
 							 <?php get_template_part( 'consul-indexparts-main' ); ?>
-		
+						
 							 <?php get_template_part( 'index-widget' ); ?>
 						</div>
 							<div class="col-md-3 col-md-pull-6">
