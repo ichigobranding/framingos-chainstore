3e6ece72b9b72da7d9b58144d1de19f4f77c8078
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-09 13:13:18 +0900

    修正

--- wp-content/themes/flamingos-chainstore-unite/single-merumaga.php
+++ wp-content/themes/flamingos-chainstore-unite/single-merumaga.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * Theme: Flat Bootstrap
- * 
- * The Template for displaying all single posts.
+ * The main template file.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
- * @package flat-bootstrap
+ * @package unite
  */
 
 get_header(); ?>
