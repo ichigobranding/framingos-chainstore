04c32ebe28bd34f229908e177be4c69168fb61fe
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-11 10:10:35 +0900

    個別募集設定完了

--- wp-content/themes/flamingos-chainstore-unite/consul-indexparts-main.php
+++ wp-content/themes/flamingos-chainstore-unite/consul-indexparts-main.php
@@ -6,36 +6,44 @@
               </h2>
                 <div id="topnewsBox-new">
                  <div class="skinMessageBoard3-s" >
-                  <ul>
-                      <?php
-                      $args = array('numberposts' => 5, 'category' => '144,10', 'orderby' => 'date', 'order' => 'asc');
-                      $rand_posts = get_posts($args);
-                      $ncount = 0;
-                      $liClass = "";
-                      foreach($rand_posts as $post) :
-                                                $ncat = get_the_category();
-                                                switch ($ncat[0]->term_id) {
-                                                    case 144:
-                                                    case 10:
-                                                        $nname = '個別相談';
-                                                        $nclass = 'newsTab-consul';
-                                                        break;
-                                                }
-                          $ncount = ++$ncount;
-                          if($ncount == 6){ $liClass = ' class="last2"'; }
-                          $ndate = $post->post_date;
-                      ?>
+                <ul class="sumawaku2">
+                  <?php
+                      $wp_query = new WP_Query();
+                      $param = array(
+                          'posts_per_page' => '7', //表示件数。-1なら全件表示
+                          'post_type' => 'consul', //カスタム投稿タイプの名称を入れる
+                          'post_status' => 'publish', //取得するステータス。publishなら一般公開のもののみ
+                          'order' => 'ASC'
+                      );
+                      $wp_query->query($param);
+                      if($wp_query->have_posts()): while($wp_query->have_posts()) : $wp_query->the_post();
+                  ?>
                     <li<?= $liClass; ?>>
                                   <table class="magatable">
                                     <tbody>
                                       <tr>
                                         <td class="daymaga-new">
-                                          <?= get_post_meta($post->ID, 'date', true); ?> 開催　
+                                          <?php the_field('consulday'); ?> 開催　
                                         </td>
                                         <td class="daymaga2-new">
-                                          <span class="topnewsTab <?= $nclass; ?>"><?= $nname; ?></span>
-                                          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                                          <a href="<?php echo esc_url( home_url( '/' ) ); ?>category/spot-consulting" title="<?php the_title(); ?>">
+                                        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                          <span class="topnewsTab <?= $nclass; ?>">
+                                                          <!-- 募集終了 -->
+                                                            <?php
+                                                                   if( get_field('noform') )
+                                                                  {
+                                                                  echo '【募集終了】';
+                                                                  }
+                                                                  else
+                                                                  {
+                                                                  echo '';
+                                                                  }
+                                                            ?>
+                                                          <!-- 募集終了 -->
+                                          </span>
+                                          
+                                          <!--<a href="<?//php echo esc_url( home_url( '/' ) ); ?>/archives/form/consul" title="<?php the_title(); ?>">-->
+                                          
                                             <?php echo mb_substr(strip_tags($post-> post_title),0,32).'...'; ?>
                                           </a>
                                           <?php get_template_part( 'new' ); ?>
@@ -44,12 +52,12 @@
                                     </tbody>
                                   </table>
                     </li>
-                      <?php endforeach; ?>
+                      <?php endwhile; endif; ?>
 
                   </ul>
-                  <div class="iblistLink2">
+                   <!-- <div class="iblistLink2">
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>category/spot-consulting">募集日程一覧を見る</a>
-                  </div>
+                  </div>-->
                  </div>
                 </div>
    <!-- コンサル一覧 -->
--- wp-content/themes/flamingos-chainstore-unite/consul-mainparts.php
+++ wp-content/themes/flamingos-chainstore-unite/consul-mainparts.php
@@ -1,19 +1,6 @@
-                             <?php
-                                     if( get_field('noform') )
-                                    {
-                                    echo '<button type="button" class="btn btn-danger">締め切りました</button>';
-                                    }
-                                    else
-                                    {
-                                    }
-                              ?>
-                              <!-- 満席 -->       
- <!--<div class="semititletopheader">こんな方々がコンサルティング（相談）にいらしています。 </div>
-<?php the_field('conwho'); ?>
-<div class="semititletopheader">コンサルティングを受けるメリット </div>
-<?php the_field('consulplus'); ?>-->
-<table class="table">
-            <tr class="tabeletitlebg"><th colspan="2">コンサルティング概要</th></tr>
+<?php the_field('consulnakami'); ?>
+<table class="table-bosyuu">
+            <tr class="tabeletitlebg"><th colspan="2">募集概要</th></tr>
             <tr>
             <td class="td_nowrap tabletitle" scope="row">開催日時</td>
             <td><span class="conday"><?php the_field('consulday'); ?><?php the_field('constart'); ?></span></td>
@@ -27,20 +14,19 @@
             <td><?php the_field('conadd'); ?><?php the_field('semiadd'); ?></td>
             </tr>
             <tr>
-            <td class="td_nowrap" scope="row">コンサルティング料</td>
+            <td class="td_nowrap" scope="row">料金</td>
             <td><?php the_field('consulpay'); ?></td>
             </tr>
             <tr>
             <td class="td_nowrap" scope="row">お支払方法</td>
-            <td>・コンサルティング料のお支払い方法は「お振込」または「カード決済」です。<br>
-・お振込口座及びカード決済の手続きにつきましては、お申込み後メールでご案内します。なおカード決済はPayPalのサイトより行っていただきます。あらかじめご了承ください。※PayPalサイト → <a href="https://www.paypal.jp/jp/home/" target="_blank">https://www.paypal.jp/jp/home/</a></br>
-<a href="<?php bloginfo('template_directory');?>/contact">・コンサルティング料のお支払方法等に何かご不明な点、ご質問がありましたらお気軽にお問い合わせください。</a>
+            <td><?php the_field('payway'); ?>
+                  <a href="get_stylesheet_directory_uri()/contact">何かご不明な点、ご質問がありましたらお気軽にお問い合わせください。</a>
             </td>
             </tr>
             <tr>
             <td class="td_nowrap" scope="row">キャンセルポリシー</td>
             <td>
-コンサルティング料をお支払いただいた後のキャンセルは、コンサルティング日の前々日までにキャンセルのご連絡をいただいた場合、コンサルティング料を返金させていただきます。あらかじめご了承ください。
+                  <?php the_field('kpr'); ?>
             </td>
             </tr>
             <tr>
@@ -56,22 +42,36 @@
             <td><?php the_field('conbiko'); ?></td>
             </tr>
             <tr><td>お問い合わせ</td>
-            <td><a href="<?php bloginfo('template_directory');?>/contact">こちらのメールフォーム</a>にてお問合せ下さい。</br>
+            <td><a href="get_stylesheet_directory_uri()/contact">こちらのメールフォーム</a>にてお問合せ下さい。</br>
             </td>
             <tr class="tabeletitlebg"><th colspan="2"></th></tr>
             </table>
                   <div class="formcss">
-                        <div class="naiyou2"><span class="glyphicon glyphicon-pencil icontopp"></span>コンサルティングの予約お申込みはこちらからお願いいたします。</div>
+                        <!--<div class="naiyou2"><span class="glyphicon glyphicon-pencil icontopp"></span>コンサルティングの予約お申込みはこちらからお願いいたします。</div>-->
                   </div>   
-				<!-- 満席 -->
-						<?php
-						if( get_field('noform') )
-						{
-						    echo '<div class="manseki">募集終了</div>';
-						}
-						else
-						{
-						    the_field('consulform');
-						}
-						?>
-				<!-- 満席 -->
+
+                        <!-- 募集終了 -->
+                            <?php
+                            if( get_field('noform') )
+                            {
+                                echo '<div class="manseki">募集終了</div>
+                              <br>
+                              募集を終了いたしました。<br><br>
+                              募集は、メルマガにて優先的にご案内しております。<br><br>
+                              <b><a href="get_stylesheet_directory_uri()/mailmag">メルマガのご登録はこちらです。</a></b><br><br><br>';
+                            }
+                            else
+                            if( get_field('semiend') )
+                            {
+                                echo '<div class="manseki">満席のため締め切りました</div>
+                              <br>
+                              募集を終了いたしました。<br><br>
+                              募集は、メルマガにて優先的にご案内しております。<br><br>
+                              <b><a href="get_stylesheet_directory_uri()/mailmag">メルマガのご登録はこちらです。</a></b><br><br><br>';
+                            }
+                            else
+                            {
+                                the_field('conform22'); 
+                            }
+                            ?>
+                         <!-- 募集終了 -->
