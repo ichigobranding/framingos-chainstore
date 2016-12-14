425fac66af6eca38761b2a196badc104f38a3155
Ichigo-Branding <ichigo.pbi@gmail.com>
2016-12-14 17:30:21 +0900

    テーマ一覧ページ設定

--- wp-content/themes/flamingos-chainstore-unite/sidebar.php
+++ wp-content/themes/flamingos-chainstore-unite/sidebar.php
@@ -38,27 +38,6 @@
 <div class="clear"></div>
 
 
-  <!--    <?php if ( ! dynamic_sidebar( 'Sidebar' ) ) : ?>
-
-      <div id="search" class="widget widget_search">
-        <br />
-        <?php get_search_form(); ?>
-      </div>
-
-      <div id="pages" class="widget widget_pages">
-        <h2 class="widget-title"><?php _e( 'Site Content', 'flat-bootstrap' ); ?></h2>
-        <ul>
-          <?php wp_list_pages( array( 'title_li' => '' ) ); ?>
-        </ul>
-      </div>
-
-      <div id="tag_cloud" class="widget widget_tag_cloud">
-        <h2 class="widget-title"><?php _e( 'Categories', 'flat-bootstrap' ); ?></h2>
-          <?php wp_tag_cloud( array( 'separator' => ' ', 'taxonomy' => 'category' ) ); ?>
-      </div>
-
-    <?php endif; // end sidebar widget area ?>-->
-
   </div><!-- #secondary　最後 -->
 
 
