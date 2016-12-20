1769ed612a81650b0f855a77036da32c76e8db68
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2016-12-20 16:19:31 +0900

    新着情報の　メルマガ・お知らせを修正

--- wp-content/themes/flamingos-chainstore-unite/icon.php
+++ wp-content/themes/flamingos-chainstore-unite/icon.php
@@ -1,8 +1,5 @@
 <span class="<?php $terms = wp_get_object_terms($post->ID,'message'); foreach($terms as $term){echo $term->slug . '';} ?>">
-<?php
-$args = array('fields'=>'names'); // オプション
-$terms = wp_get_post_terms( $post->ID, 'message', $args);
-?>
 
+<?php $terms = wp_get_object_terms($post->ID,'message'); foreach($terms as $term){echo $term->name . '';} ?>
 
 </span> 
