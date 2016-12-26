2567385ba9c5587bb45dc6ebdca5649f4b29a4d9
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2016-12-26 10:53:47 +0900

    プラグイン削除

--- wp-content/plugins/storefront-powerpack/includes/customizer/header/assets/js/sp-sticky-header.js
+++
@@ -1,9 +0,0 @@
-( function( $ ) {
-	$(window).load( function() {
-		var headerHeight = $( '.site-header' ).outerHeight();
-
-		if ( $(window).width() > 768 ) {
-			$( '.site' ).css( 'padding-top', headerHeight );
-		}
-	});
-} )( jQuery );
