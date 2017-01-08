1024f78d4dd589007037c7d1699571217384e98d
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2017-01-08 11:40:25 +0900

    プログラムアップデート

--- wp-content/themes/flamingos-chainstore-unite/image.php
+++ wp-content/themes/flamingos-chainstore-unite/image.php
@@ -7,20 +7,17 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area image-attachment col-sm-12 col-md-8 <?php echo of_get_option( 'site_layout' ); ?>">
+<center>
+	<div id="primary" class="content-area image-attachment col-sm-12 col-md-12 <?php echo of_get_option( 'site_layout' ); ?>">
 		<div id="content" class="site-content" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<h1 class="entry-title"><?php the_title(); ?></h1>
+					
 
-					<div class="entry-meta">
-						<?php unite_posted_on(); ?>
-					</div><!-- .entry-meta -->
-
+		
 					<nav role="navigation" id="image-navigation" class="navigation-image nav-links">
 						<div class="nav-previous"><?php previous_image_link( false, __( '<i class="fa fa-chevron-left"></i> Previous', 'unite' ) ); ?></div>
 						<div class="nav-next"><?php next_image_link( false, __( 'Next <i class="fa fa-chevron-right"></i>', 'unite' ) ); ?></div>
@@ -50,23 +47,13 @@ get_header();
 								}
 								$k++;
 								// If there is more than 1 attachment in a gallery
-								if ( count( $attachments ) > 1 ) {
-									if ( isset( $attachments[ $k ] ) )
-										// get the URL of the next image attachment
-										$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
-									else
-										// or get the URL of the first image attachment
-										$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
-								} else {
-									// or, if there's only 1 image, get the URL of the image
-									$next_attachment_url = wp_get_attachment_url();
-								}
+			
 							?>
 
-							<a href="<?php echo $next_attachment_url; ?>" title="<?php the_title_attribute(); ?>" rel="attachment"><?php
+							<?php
 								$attachment_size = apply_filters( 'unite_attachment_size', array( 1200, 1200 ) ); // Filterable image size.
 								echo wp_get_attachment_image( $post->ID, $attachment_size );
-							?></a>
+							?>
 						</div><!-- .attachment -->
 
 						<?php if ( ! empty( $post->post_excerpt ) ) : ?>
@@ -90,16 +77,10 @@ get_header();
 				</footer><!-- .entry-meta -->
 			</article><!-- #post-<?php the_ID(); ?> -->
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() )
-					comments_template();
-			?>
 
-		<?php endwhile; // end of the loop. ?>
 
+		<?php endwhile; // end of the loop. ?></center>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
