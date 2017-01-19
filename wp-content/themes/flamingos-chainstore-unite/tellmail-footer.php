d2be36839ffb158b48f98765f008d827dd1b02fa
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2017-01-19 15:08:38 +0900

    お気軽にお問い合わせください　に変更

--- wp-content/themes/flamingos-chainstore-unite/tellmail-footer.php
+++ wp-content/themes/flamingos-chainstore-unite/tellmail-footer.php
@@ -1,6 +1,6 @@
 									<!-- 連絡先 -->
 												<div class="tellwaku-footer">
-													<span class="uke2">地域名・キーワード</span><br>
+													<span class="uke2">お気軽にお問い合わせください</span><br>
 													<span class="glyphicon glyphicon-earphone telcon" aria-hidden="true"></span><a href="tel:0120916625">0120-916-625</a><br>
 													<span class="glyphicon glyphicon-envelope telcon" aria-hidden="true"></span><a href="<?php echo esc_url( home_url( '/' ) ); ?>/contact">メール受付はコチラ</a><br>
 													<span class="uke">受付：平日10時〜16時（土日祝除く）</span><br>
