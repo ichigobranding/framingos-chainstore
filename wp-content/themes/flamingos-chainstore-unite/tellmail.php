1250a1a9b73146464abf72469c13ea3c29900d9f
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2017-03-27 22:11:40 +0900

    電話連絡　変更

--- wp-content/themes/flamingos-chainstore-unite/tellmail.php
+++ wp-content/themes/flamingos-chainstore-unite/tellmail.php
@@ -1,9 +1,8 @@
 									<!-- 連絡先 -->
 												<div class="tellwaku">
 													<span class="uke2">お気軽にお問い合わせください</span><br>
-													<span class="glyphicon glyphicon-earphone telcon" aria-hidden="true"></span><a href="tel:0120916625">0120-916-625</a><br>
+													<span class="glyphicon glyphicon-earphone telcon" aria-hidden="true"></span><a href="tel:0●●●●●●">●●●●-●●●-●●●</a><br>
 													<span class="glyphicon glyphicon-envelope telcon" aria-hidden="true"></span><a href="<?php echo esc_url( home_url( '/' ) ); ?>/contact">メール受付はコチラ</a><br>
-													<span class="uke">受付：平日10時〜16時（土日祝除く）</span><br>
+													<span class="uke">受付：平日●時〜●時（土日祝除く）</span><br>
 												</div>
-						
 									<!-- 連絡先 -->
