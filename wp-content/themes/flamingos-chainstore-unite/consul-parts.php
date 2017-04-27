13e380c8a3ba602fa5bd1816aed29a53d48e25df
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2017-04-27 11:17:29 +0900

    個別指導一覧ページ

--- wp-content/themes/flamingos-chainstore-unite/consul-parts.php
+++ wp-content/themes/flamingos-chainstore-unite/consul-parts.php
@@ -1,14 +1,14 @@
                              <?php
                                      if( get_field('noform') )
                                     {
-                                    echo '<button type="button" class="btn btn-danger">締め切りました</button>';
+                                    echo '<p><span style="color: #ff0000;"><strong>【募集終了】</strong></span></p>';
                                     }
                                     else
                                     {
                                     }
                               ?>
                               <!-- 満席 -->
-                    <div class="conpa">   
+                    <div class="conpa">
                       <p>
                         <span class="pinkfont">日時：<?php the_field('consulday'); ?><?php the_field('constart'); ?></span><br>
                         場所：<?php the_field('consulplace'); ?><br>
