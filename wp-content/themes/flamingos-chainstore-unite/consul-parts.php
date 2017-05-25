d73a61674a63178f2ef421268bab8d47aa59eb62
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2017-05-25 22:14:32 +0900

    アップデート

--- wp-content/themes/flamingos-chainstore-unite/consul-parts.php
+++ wp-content/themes/flamingos-chainstore-unite/consul-parts.php
@@ -1,4 +1,4 @@
-                             <?php
+                              <?php
                                      if( get_field('noform') )
                                     {
                                     echo '<p><span style="color: #ff0000;"><strong>【募集終了】</strong></span></p>';
@@ -12,6 +12,7 @@
                       <p>
                         <span class="pinkfont">日時：<?php the_field('consulday'); ?><?php the_field('constart'); ?></span><br>
                         場所：<?php the_field('consulplace'); ?><br>
-                        受講料：10,800円(税込) ※お支払方法は「お振込」または「カード決済」です。
+                        受講料：<?php the_field('consulpay'); ?><br>
+                        ※お支払方法は「お振込」または「カード決済」です。
                       </p>
                     </div>
