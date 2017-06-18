610bca9e16fdcc4def173780888116ea53440ecc
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2017-06-18 10:31:32 +0900

    セミナー募集中＆過去の一覧機能追加

--- wp-content/themes/flamingos-chainstore-unite/semi-indexparts-main.php
+++ wp-content/themes/flamingos-chainstore-unite/semi-indexparts-main.php
@@ -6,58 +6,47 @@
                 <div id="topnewsBox-new">
                  <div class="skinMessageBoard3-s" >
                   <ul>
-                    <?php
-                      $wp_query = new WP_Query();
-                      $param = array(
-                          'posts_per_page' => '7', //表示件数。-1なら全件表示
-                          'post_type' => 'semi', //カスタム投稿タイプの名称を入れる
-                          'post_status' => 'publish', //取得するステータス。publishなら一般公開のもののみ
-                          'order' => 'ASC'
-                      );
-                      $wp_query->query($param);
-                      if($wp_query->have_posts()): while($wp_query->have_posts()) : $wp_query->the_post();
-                  ?>
-                      <li<?= $liClass; ?>>
-                                  <table class="magatable">
-                                    <tbody>
-                                      <tr>
-                                        <td class="daymaga-new">
-                                         <span><?php the_field('semiday'); ?> <span>開催</span>
-                                        </td>
-                                        <td class="daymaga2-new">
-                                          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                                           <span class="topnewsTab <?= $nclass; ?>">
-                                                          <!-- 満席 -->
-                                                            <!--<?//php
-                                                                   //if( get_field('noform') )
-                                                                  {
-                                                                  //echo '<span class="syuryo-icon">募集終了</span>';
-                                                                  }
-                                                                 //else
-                                                                  //if( get_field('semiend') )
-                                                                  {
-                                                                  //echo '<span class="syuryo-icon">満席</span>';
-                                                                  }
-                                                                  //else
-                                                                  {
-                                                                  //echo '';
-                                                                  }
-                                                            ?>-->
-                                                          <!-- 満席 -->
-                                          </span>                                          
-                                            <?php echo mb_substr(strip_tags($post-> post_title),0,32).'...'; ?>
-                                          </a>
-                                          <?php get_template_part( 'new' ); ?>
-                                      </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                      </li>
-                     <?php endwhile; endif; ?>
-                  </ul>
-                  <!-- <div class="iblistLink2">
-                    <a href="<?//php echo esc_url( home_url( '/' ) ); ?>/semi">開催一覧を見る</a>
-                  </div>-->
+                        <?php $args = array(
+                            'posts_per_page' => '15', //表示件数。-1なら全件表示
+                            'post_status' => 'publish', //取得するステータス。publishなら一般公開のもののみ
+                            'order' => 'DESC',
+                            'post_type' => 'semi', //カスタム投稿タイプ
+                            'tax_query' => array( //タクソノミー、タームの設定
+                             array(
+                            'taxonomy' => 'semibo', //タクソノミー名
+                            'field' => 'slug', //基本的にslugに指定します（必須）
+                            'terms' => 'semiup' //ターム（スラッグ名）
+                          )
+                        )
+                        );
+                        $domestic_post = get_posts($args);
+                        if($domestic_post) : foreach($domestic_post as $post) : setup_postdata( $post ); ?>
+                       <li class="magawaku">
+                                                  <table class="magatable">
+                                                    <tbody>
+                                                      <tr>
+                                                        <td class="daymaga-new"><?php the_field('semiday'); ?>　開催</td>
+                                                        <td class="daymaga2-new">
+                                                        <a href="<?php the_permalink(); ?>">
+                                                          <?php the_title(); ?>
+                                                        </a>
+                                                        <?php get_template_part( 'new' ); ?>
+                                                      </td>
+                                                      </tr>
+                                                    </tbody>
+                                                  </table>
+                            </li>
+                          <?php endforeach; ?>
+                        <?php else : //記事が存在しない場合 ?>
+                        <p>表示する記事がありません。</p>
+                          <?php endif;
+                          wp_reset_postdata(); ?>
+                    </ul>
+                       <div class="iblistLink">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>/semiuplist">
+                              一覧を見る
+                        </a>
+                      </div>
                  </div>
                 </div>
                 <!-- セミナー -->
