908356b8300a3be3ebc66091c1bf9eb3c1828e64
田中総一郎 <ichigobranding@tanakasouichirounoMacBook-Pro.local>
2017-06-21 22:19:42 +0900

    アピールバナー

--- wp-content/themes/flamingos-chainstore-unite/ap-bana.php
+++ wp-content/themes/flamingos-chainstore-unite/ap-bana.php
@@ -4,7 +4,7 @@
 	</a>
 </div>
 <div class="banapadding">
-	<a href="※">
-		<img src="<?php echo get_stylesheet_directory_uri() ?>/images/bana-mid.png" alt=""/>
+	<a href="https://ichigo-branding.com/semi/ameblo-2/">
+		<img src="<?php echo get_stylesheet_directory_uri() ?>/images/bana-mid2.png" alt=""/>
 	</a>
 </div>
