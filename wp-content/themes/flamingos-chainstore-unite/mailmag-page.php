1e8e221e3778b9e6cf3a57b8cb7b349aa522f628
田中総一郎 <>
2018-04-25 05:52:55 +0900

    メルマガページ調整

---
+++ wp-content/themes/flamingos-chainstore-unite/mailmag-page.php
@@ -0,0 +1,23 @@
+<div class="row mailmagpadding">
+  <div class="col-xs-12 col-md-12">
+<!-- メルマガURL -->
+<form action="https://24auto.biz/ichigo-branding/planmail.php" method="post" enctype="multipart/form-data">
+<!-- メルマガURL -->
+<input type="hidden" name="mcode" value="UTF-8">
+<input type="hidden" name="pid" value="14"><!-- メルマガの番号 -->
+<input type="hidden" name="spflg" value="1">
+<table width="520" cellpadding="3" cellspacing="1" border="0" bgcolor="#aaaaaa" class="OM_table">
+<tr>
+<td align="center" colspan="2" bgcolor="#e5e5e5" class="mailmagfomtitle"><b>アメブロをカスタマイズしてあなたの価値を高める方法（登録無料）</b><br></td>
+</tr>
+<tr>
+<td bgcolor="#fafafa" align="center" width="120"><b>メールアドレス</b><strong style="color:#FF0000;"><span class="fna">*</span></strong></td>
+<td class="mailmagfom" bgcolor="#fafafa" width="400"> <input type="text" name="email" size="30" class="bizfom2-2">
+</td>
+</tr>
+  <input type="hidden" name="fld1" value="ホームページより登録いただきました。">
+</table>
+<center><input type="submit" name="sbm" value="登録"></center>
+</form>
+  </div>
+</div>
