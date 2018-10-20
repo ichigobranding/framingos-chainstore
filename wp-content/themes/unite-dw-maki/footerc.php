d4ebcf76fbf2bd51ed159a16a690294421479131
田中総一郎 <>
2018-10-20 17:50:33 +0900

    屋号のロゴを変更

--- wp-content/themes/unite-dw-maki/footerc.php
+++ wp-content/themes/unite-dw-maki/footerc.php
@@ -1,8 +1,8 @@
 <div class="row sitemaprap">
 	  <div class="col-xs-12 col-md-4 sitemaprap-l">
-	  	<img src="<?php echo get_stylesheet_directory_uri() ?>/images/top-bana.png" alt="ロゴ"/>
+	  	<img src="<?php echo get_stylesheet_directory_uri() ?>/images/top-bana-footer.jpg" alt="ロゴ"/>
 	  	<div class="tellwaku2 well2">
-	  	<p><span class="kabu">企業名・屋号</span></p>
+	  	<p><div class="kabu">企業名・屋号</div></p>
 	  	<p>〒000-0000</p>
 		<p>住所</p>
 	  	<p><a href="tel:00000000">Tel:00-0000-0000（受付：平日00時～00時）</a><p>
@@ -22,7 +22,7 @@
 				<ul class="footercli">
 					<li class="sitelili"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/※">テキスト</a></li>
 					<li class="sitelili"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/access">アクセス</a></li>
-					<li class="sitelili"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/contact">お問い合わせフォーム</a>テキスト</a></li>
+					<li class="sitelili"><a href="<?php echo esc_url( home_url( '/' ) ); ?>/contact">お問い合わせフォーム</a></li>
 				</ul>
 			 </div>
 		</div><!-- row　-->
